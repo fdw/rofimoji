14a45e08cfb2b332602fd7212bd19e5bd5e6c3af
Manuel Esberger <esberger.manuel@live.at>
2018-02-23 18:56:58 +0100

    fixed extract_emojis.py url and updated emojis in rofimoji.py

--- extract_emojis.py
+++ extract_emojis.py
@@ -1,17 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = requests.get('https://emojipedia.org/emoji/')  # type: requests.Response
-soup = BeautifulSoup(data.content, 'lxml')  # type: BeautifulSoup
+datas = [
+    'https://emojipedia.org/people/',
+    'https://emojipedia.org/nature/',
+    'https://emojipedia.org/food-drink/',
+    'https://emojipedia.org/activity/',
+    'https://emojipedia.org/travel-places/',
+    'https://emojipedia.org/objects/',
+    'https://emojipedia.org/symbols/',
+    'https://emojipedia.org/flags/']
 
 python_file = open('emojis.py', 'w')
 python_file.write('emojis="""\n')
 
-for table_row in soup.find_all('tr'):
-    emoji_row = table_row.find('td')
-    text = emoji_row.get_text()
+for url in datas:
+    data = requests.get(url)  # type: requests.Response
+    soup = BeautifulSoup(data.content, 'lxml')  # type: BeautifulSoup
 
-    python_file.write(text + '\n')
+    soup = soup.find("div", attrs={"class": "content"})
+    for table_row in soup.find_all('li'):
+        emoji_row = table_row.find('a')
+        text = emoji_row.get_text()
+
+        python_file.write(text + '\n')
+
 python_file.write('"""')
 
 python_file.close()

ba96e61161f5ced0422edf7c33c44895a341e56a
Fabian Winter <5821180+fdw@users.noreply.github.com>
2020-08-22 20:55:19 +0200

    Make version a snapshot

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rofimoji',
-    version='4.3.0',
+    version='4.3.0-SNAPSHOT',
     description='Simple character picker using rofi',
     author='fdw',
     author_email='5821180+fdw@users.noreply.github.com',

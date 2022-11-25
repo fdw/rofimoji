07acefba05530a8f354eefef33a2c9eef8d688df
Fabian Winter <5821180+fdw@users.noreply.github.com>
2022-11-25 11:52:31 +0100

    Fix version display
    Issue: #139

--- src/picker/__init__.py
+++ src/picker/__init__.py
@@ -1,9 +1,5 @@
 from importlib import metadata
-from importlib.metadata import PackageNotFoundError
 
-try:
-    __version__ = metadata.version(__package__)
-except PackageNotFoundError:
-    __version__ = "dirty"
+__version__ = metadata.version("rofimoji")
 
 del metadata

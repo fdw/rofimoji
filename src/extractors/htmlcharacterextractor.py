5d7d15e79996cee3d304b1a731baee76053bc23e
Arnout Engelen <arnout@bzzt.net>
2024-08-15 12:43:41 +0200

    Add HTML characters
    like 'eacute' etc.
    
    'Character' would crash on characters that consist of multiple
    code points. I added the assumption that in this case the
    additional 'combining characters' would not impact the bidi
    class.

---
+++ src/extractors/htmlcharacterextractor.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+import json
+from pathlib import Path
+import requests
+from typing import List
+
+from .extractor import Extractor
+
+@dataclass
+class HtmlCharacter:
+    string: str
+    name: str
+
+class HtmlCharacterExtractor(Extractor):
+    __characters: List[HtmlCharacter]
+
+    def __init__(self):
+        self.__characters = []
+
+    def __fetch_data(self) -> None:
+        response: requests.Response = requests.get(
+            "https://html.spec.whatwg.org/entities.json", timeout=120
+        )
+        entities = json.loads(response.text)
+        for key in entities:
+            if key.endswith(';') and key != "&NewLine;":
+                name = key[1:-1]
+                char = ''.join(list(map(chr, entities[key]['codepoints'])))
+                self.__characters.append(HtmlCharacter(char, name))
+
+    def __write_to_file(self, target: Path) -> None:
+        with (target / "html_characters.csv").open("w") as symbol_file:
+            for character in self.__characters:
+                symbol_file.write(f"{character.string} {character.name}\n")
+
+    def extract_to(self, target: Path) -> None:
+        self.__fetch_data()
+        self.__write_to_file(target)
